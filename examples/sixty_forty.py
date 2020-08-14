1a9b00fde1578270c450a7d432f2f7fb6e24637f
Jun Ma <junma018@outlook.com>
2020-08-14 10:46:48 +0800

    Provide feasible dataset config for running the examples.

--- examples/sixty_forty.py
+++ examples/sixty_forty.py
@@ -13,8 +13,8 @@ from qstrader.trading.backtest import BacktestTradingSession
 
 
 if __name__ == "__main__":
-    start_dt = pd.Timestamp('2003-09-30 14:30:00', tz=pytz.UTC)
-    end_dt = pd.Timestamp('2019-12-31 23:59:00', tz=pytz.UTC)
+    start_dt = pd.Timestamp('2007-01-30 14:30:00', tz=pytz.UTC)
+    end_dt = pd.Timestamp('2015-12-31 23:59:00', tz=pytz.UTC)
 
     # Construct the symbols and assets necessary for the backtest
     strategy_symbols = ['SPY', 'AGG']
